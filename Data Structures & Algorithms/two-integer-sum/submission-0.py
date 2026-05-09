6266b9accbeabd27ea7dd529b838a6391282c89b
Kyre Shamwell <159083798+kyreshamwell@users.noreply.github.com>
2026-05-08 21:30:36 -0500

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        hashmap = {}
+
+        for i, num in enumerate(nums):
+            seen = target - num
+            if seen in hashmap:
+                return [hashmap[seen], i]
+            hashmap[num] = i
+        
+
+                 
+        
